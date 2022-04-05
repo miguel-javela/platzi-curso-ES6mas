c483e086b3dd701bb95e88304bb4773ed405dba8
miguel javela <migueljavela2014@gmail.com>
2022-04-04 22:03:22 -0500

    clases, modulos y generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -118,4 +118,40 @@ helloPromise()
   .then(response => console.log(response))
   .catch(error => console.log(error));
 
-  
+
+//clases
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+
+//modulos
+import { hello } from './module';
+
+hello();
+
+
+//generators
+function* helloWorld() {
+  if (true) {
+    yield 'Hello, ';
+  }
+  if (true) {
+    yield 'World';
+  }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
