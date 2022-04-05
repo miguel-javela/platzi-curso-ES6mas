f0e036d44a0cc2c5618d261227d85d449b30bb5e
miguel javela <migueljavela2014@gmail.com>
2022-04-04 20:56:46 -0500

    arrow function, promesas, propiedad de objeto mejorada

--- src/es6/index.js
+++ src/es6/index.js
@@ -6,13 +6,13 @@ function newFunction(name, age, country) {
     console.log(name, age, country);
 }
 
-  // es6
+// es6
 function newFunction2(name = 'oscar', age = 32, country = "MX") {
-    console.log(name, age, country);
-  };
-  
-  newFunction2();
-  newFunction2('Ricardo', '23', 'CO');
+  console.log(name, age, country);
+};
+newFunction2();
+newFunction2('Ricardo', '23', 'CO');
+
 
 //concatenar  
 let hello = "Hello";
@@ -22,6 +22,7 @@ console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
 
+
 //multilinea
 let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
   + "otra frase epica que necesitamos."
@@ -30,9 +31,9 @@ let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
 let lorem2 = `otra frase epica que necesitamos
 ahora es otra frase epica
 `;
-
 console.log(lorem);
 console.log(lorem2);
+
 
 //desestructuracion
 let person = {
@@ -40,31 +41,81 @@ let person = {
   'age': 32,
   'country': 'MX'
 }
-
 console.log(person.name, person.age);
 
 let { name, age } = person;
 console.log(name, age);
+
 
 //spread operator
 let team1 = ['Oscar', 'Julian', 'Ricardo'];
 let team2 = ['Valeria', 'Yesica', 'Camila'];
 
 let education = ['David', ...team1, ...team2];
-
 console.log(education);
 
+
+//let y var
 {
   var globalVar = "Global Var";
 }
-
 {
   let globalLet = 'Global Let';
   console.log(globalLet);
 }
-
 console.log(globalVar);
 
 const a = 'b';
 a = 'a';
 console.log(a);
+
+
+//propiedad de objetos mejorada
+name = 'oscar';
+age = 32;
+
+//es5
+obj = { name: name, age: age };
+//es6
+obj2 = { name, age };
+console.log(obj2);
+
+
+//arrow function
+const names = [
+  { name: 'Oscar', age: 32 },
+  { name: 'Yesica', age: 27 }
+]
+
+let listOfNames = names.map(function (item) {
+  console.log(item.name);
+})
+//es6
+let listOfNames2 = names.map(item => console.log(item.name));
+
+const listOfNames3 = (name, age, country) => {
+  //codigo... 
+}
+
+const listOfNames4 = name => {
+  //codigo...
+}
+const square = num => num * num;
+
+
+//promesas
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (false) {
+      resolve('Hey!');
+    } else {
+      reject('Ups!!');
+    }
+  });
+}
+
+helloPromise()
+  .then(response => console.log(response))
+  .catch(error => console.log(error));
+
+  
