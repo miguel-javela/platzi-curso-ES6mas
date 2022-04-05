84cf4e19fd846cac752eb649b7a45502d8e1c104
miguel javela <migueljavela2014@gmail.com>
2022-04-04 22:58:41 -0500

    async await

--- src/es8/index.js
+++ src/es8/index.js
@@ -27,3 +27,31 @@ console.log('food'.padEnd(12, '  -----'))
 const obj = {
     name: 'oscar',
 }
+
+
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Test Error'))
+    })
+};
+  
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+  
+helloAsync();
+  
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+  
+anotherFunction();
